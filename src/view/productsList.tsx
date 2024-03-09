c6969b2ba15b1661ff3428e4997f5df538f91b1c
Asif Imran <imransecrets@gmail.com>
2024-03-09 06:16:53 -0800

    website project

--- src/view/productsList.tsx
+++ src/view/productsList.tsx
@@ -1,16 +1,16 @@
 
 import ProductCard from '@/components/productCard';
-import P1 from '/public/p1.png'
-import P2 from '/public/p2.png'
-import P3 from '/public/p3.png'
+import p1 from '/public/p1.png'
+import p2 from '/public/p2.png'
+import p3 from '/public/p3.png'
 
 
 const ProductsList = () => {
   return (
     <div className=' flex justify-evenly mt-16'>
-       <ProductCard img = {P1}  title = 'Flex SweatShirt' price = "$345"/>
-       <ProductCard img = {P2}  title = 'Brushed Ragalan SweatShirt' price = "$456"/>
-       <ProductCard img = {P3}  title = 'Lite SweatShirt' price = "$242"/>
+       <ProductCard img = {p1}  title = 'Flex SweatShirt' price = "$345"/>
+       <ProductCard img = {p2}  title = 'Brushed Ragalan SweatShirt' price = "$456"/>
+       <ProductCard img = {p3}  title = 'Lite SweatShirt' price = "$242"/>
     </div>
   )
 }
